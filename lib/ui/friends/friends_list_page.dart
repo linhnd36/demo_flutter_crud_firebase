0a9c2724bc398b8fca06266aac71661ae8a06845
LinhCN <linhcnse130515@fpt.edu.vn>
2021-03-07 00:09:17 +0700

    Add search name function

--- lib/ui/friends/friends_list_page.dart
+++ lib/ui/friends/friends_list_page.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_demo_curd_firebase/ui/frienddetails/friend_details_page.dart';
 import 'package:flutter_demo_curd_firebase/ui/friends/friend.dart';
 
+import 'friend.dart';
+
 class FriendsListPage extends StatefulWidget {
   @override
   _FriendsListPageState createState() => new _FriendsListPageState();
@@ -12,6 +14,7 @@ class FriendsListPage extends StatefulWidget {
 
 class _FriendsListPageState extends State<FriendsListPage> {
   List<Friend> _friends = [];
+  List<Friend> friends = [];
 
   @override
   void initState() {
@@ -25,6 +28,7 @@ class _FriendsListPageState extends State<FriendsListPage> {
 
     setState(() {
       _friends = Friend.allFromResponse(response.body);
+      friends = _friends;
     });
   }
 
@@ -32,15 +36,15 @@ class _FriendsListPageState extends State<FriendsListPage> {
     var friend = _friends[index];
 
     return new ListTile(
-      onTap: () => _navigateToFriendDetails(friend, index),
-      leading: new Hero(
-        tag: index,
-        child: new CircleAvatar(
-          backgroundImage: new NetworkImage(friend.avatar),
+        onTap: () => _navigateToFriendDetails(friend, index),
+        leading: new Hero(
+          tag: index,
+          child: new CircleAvatar(
+            backgroundImage: new NetworkImage(friend.avatar),
+          ),
         ),
-      ),
-      title: new Text(friend.name),
-      subtitle: new Text(friend.email),
+        title: new Text(friend.name),
+        subtitle: new Text(friend.email),
     );
   }
 
@@ -53,25 +57,62 @@ class _FriendsListPageState extends State<FriendsListPage> {
       ),
     );
   }
-
+  void findName(String placeName) async
+  {
+    _friends = friends;
+    List<Friend> friendList = [];
+     for (int i = 0; i < _friends.length; i++){
+      if (_friends[i].name.contains(placeName)){
+        friendList.add(_friends[i]);
+      }
+    }
+     setState(() {
+       _friends = friendList;
+     });
+  }
+  TextEditingController searchEditingController = TextEditingController();
+  Icon cusIcon = Icon(Icons.search);
+  Widget cusSearchBar = Text("Friends");
   @override
   Widget build(BuildContext context) {
     Widget content;
-
     if (_friends.isEmpty) {
       content = new Center(
         child: new CircularProgressIndicator(),
       );
     } else {
       content = new ListView.builder(
-        itemCount: _friends.length,
-        itemBuilder: _buildFriendListTile,
+          itemCount: _friends.length,
+          itemBuilder: _buildFriendListTile,
       );
     }
-
     return new Scaffold(
-      appBar: new AppBar(title: new Text('Friends')),
-      body: content,
+      appBar: new AppBar(
+        title: cusSearchBar,
+          actions: <Widget>[
+            IconButton(
+                icon: cusIcon,
+                onPressed: (){
+                  setState(() {
+                    if(this.cusIcon.icon == Icons.search){
+                      this.cusIcon = Icon(Icons.cancel);
+                      this.cusSearchBar = TextField(
+                          onChanged: (val){
+                            findName(val);
+                          },
+                          controller: searchEditingController
+                      );
+                    }
+                    else{
+                      this.cusIcon = Icon(Icons.search);
+                      this.cusSearchBar = Text("Friends");
+                    }
+                  });
+                }
+            )
+          ],
+      ),
+      body: content
     );
   }
 }

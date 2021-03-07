e0f5b1ac3d8dc92ad1477883319a7d8f3e558833
LinhND <linhnguyen131999@gmail.com>
2021-03-07 21:23:48 +0700

    add search

--- lib/ui/friends/friends_list_page.dart
+++ lib/ui/friends/friends_list_page.dart
@@ -17,7 +17,7 @@ class FriendsListPage extends StatefulWidget {
 
 class _FriendsListPageState extends State<FriendsListPage> {
   List<Friend> _friends = [];
-  List<Friend> friends = [];
+  List<Friend> _searchResult = [];
 
   @override
   void initState() {
@@ -38,23 +38,6 @@ class _FriendsListPageState extends State<FriendsListPage> {
             });
     })
     });
-    print(_friends);
-  }
-
-  Widget _buildFriendListTile(BuildContext context, int index) {
-    var friend = _friends[index];
-
-    return new ListTile(
-        onTap: () => _navigateToFriendDetails(friend, index),
-        leading: new Hero(
-          tag: index,
-          child: new CircleAvatar(
-            backgroundImage: new NetworkImage(friend.avatar),
-          ),
-        ),
-        title: new Text(friend.name),
-        subtitle: new Text(friend.email),
-    );
   }
 
   void _navigateToFriendDetails(Friend friend, Object avatarTag) {
@@ -66,35 +49,81 @@ class _FriendsListPageState extends State<FriendsListPage> {
       ),
     );
   }
-  void findName(String placeName) async
-  {
-    List<Friend> friendList = [];
-     for (int i = 0; i < friends.length; i++){
-      if (friends[i].name.contains(placeName)){
-        friendList.add(friends[i]);
-      }
+
+  onSearchTextChanged(String text) async {
+    _searchResult.clear();
+    if (text.isEmpty) {
+      setState(() {});
+      return;
     }
-     print(friendList);
-     setState(() {
-       _friends = friendList;
-     });
+    print("Size lsit friend:"+_friends.length.toString());
+    _friends.forEach((friend) {
+      if (friend.name.contains(text) || friend.email.contains(text))
+        _searchResult.add(friend);
+    });
+    print("Size search:"+_searchResult.length.toString());
+    setState(() {});
   }
+
+
   TextEditingController searchEditingController = TextEditingController();
   Icon cusIcon = Icon(Icons.search);
   Widget cusSearchBar = Text("Friends");
   @override
   Widget build(BuildContext context) {
-    Widget content;
-    if (_friends.isEmpty) {
-      content = new Center(
-        child: new CircularProgressIndicator(),
-      );
-    } else {
-      // content = new ListView.builder(
-      //     itemCount: _friends.length,
-      //     itemBuilder: _buildFriendListTile,
-      // );
-      content = StreamBuilder<QuerySnapshot>(
+    return new Scaffold(
+      appBar: new AppBar(
+        title: cusSearchBar,
+          actions: <Widget>[
+            IconButton(
+                icon: cusIcon,
+                onPressed: (){
+                  setState(() {
+                    if(this.cusIcon.icon == Icons.search){
+                      this.cusIcon = Icon(Icons.cancel);
+                      this.cusSearchBar = TextField(
+                          onChanged: onSearchTextChanged,
+                          controller: searchEditingController
+                      );
+                    }
+                    else{
+                      this.cusIcon = Icon(Icons.search);
+                      this.cusSearchBar = Text("Friends");
+                      searchEditingController.clear();
+                      onSearchTextChanged('');
+                    }
+                  });
+                }
+            )
+          ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => AddFriendPage(),));
+        },
+        backgroundColor: Theme.of(context).primaryColor,
+        child: Icon(Icons.add),
+      ),
+
+      body: _searchResult.length != 0 || searchEditingController.text.isNotEmpty
+        ? new ListView.builder(
+              itemCount: _searchResult.length,
+              itemBuilder: (context, index){
+                Friend currenFriend = new Friend(_searchResult[index].avatar, _searchResult[index].name, _searchResult[index].email, _searchResult[index].location);
+                return ListTile(
+                  onTap: () => _navigateToFriendDetails(currenFriend, index),
+                  leading: new Hero(
+                    tag: index,
+                    child: new CircleAvatar(
+                      backgroundImage: new NetworkImage(_searchResult[index].avatar),
+                    ),
+                  ),
+                  title: new Text(_searchResult[index].name),
+                  subtitle: new Text(_searchResult[index].email),
+                );
+              },
+          )
+          : StreamBuilder<QuerySnapshot>(
         stream: FirebaseFirestore.instance.collection("friends").snapshots(),
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> querySnapshot){
           if(querySnapshot.hasError){
@@ -105,11 +134,12 @@ class _FriendsListPageState extends State<FriendsListPage> {
           }else{
 
             final list = querySnapshot.data.docs;
-
+            _friends.clear();
             return ListView.builder(
               itemCount: list.length,
               itemBuilder: (context,index){
                 Friend currenFriend = new Friend(list[index]["avatar"], list[index]["name"], list[index]["email"], list[index]["location"]);
+                _friends.add(currenFriend);
                 return ListTile(
                   onTap: () => _navigateToFriendDetails(currenFriend, index),
                   leading: new Hero(
@@ -125,42 +155,6 @@ class _FriendsListPageState extends State<FriendsListPage> {
             );
           }
         },
-      );
-    }
-    return new Scaffold(
-      appBar: new AppBar(
-        title: cusSearchBar,
-          actions: <Widget>[
-            IconButton(
-                icon: cusIcon,
-                onPressed: (){
-                  setState(() {
-                    if(this.cusIcon.icon == Icons.search){
-                      this.cusIcon = Icon(Icons.cancel);
-                      this.cusSearchBar = TextField(
-                          onChanged: (val){
-                            findName(val);
-                          },
-                          controller: searchEditingController
-                      );
-                    }
-                    else{
-                      this.cusIcon = Icon(Icons.search);
-                      this.cusSearchBar = Text("Friends");
-                      _loadFriends();
-                    }
-                  });
-                }
-            )
-          ],
-      ),
-      body: content,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => AddFriendPage(),));
-        },
-        backgroundColor: Theme.of(context).primaryColor,
-        child: Icon(Icons.add),
       ),
     );
   }

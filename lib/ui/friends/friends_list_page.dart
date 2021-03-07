ded1ee963b52acaab27a97572767af2b3a789697
LocNT PC <kusmile15@gmail.com>
2021-03-07 20:28:46 +0700

    Giữ 1 element để delete nó

--- lib/ui/friends/friends_list_page.dart
+++ lib/ui/friends/friends_list_page.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_demo_curd_firebase/ui/friends/add_friend_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -31,13 +32,14 @@ class _FriendsListPageState extends State<FriendsListPage> {
         .collection('friends')
         .get()
         .then((QuerySnapshot querySnapshot) => {
-          querySnapshot.docs.forEach((doc) {
-            setState(() {
-              Friend friend = new Friend(doc["avatar"], doc["name"], doc["email"], doc["location"]);
-              _friends.add(friend);
+              querySnapshot.docs.forEach((doc) {
+                setState(() {
+                  Friend friend = new Friend(doc["avatar"], doc["name"],
+                      doc["email"], doc["location"]);
+                  _friends.add(friend);
+                });
+              })
             });
-    })
-    });
     print(_friends);
   }
 
@@ -45,15 +47,15 @@ class _FriendsListPageState extends State<FriendsListPage> {
     var friend = _friends[index];
 
     return new ListTile(
-        onTap: () => _navigateToFriendDetails(friend, index),
-        leading: new Hero(
-          tag: index,
-          child: new CircleAvatar(
-            backgroundImage: new NetworkImage(friend.avatar),
-          ),
+      onTap: () => _navigateToFriendDetails(friend, index),
+      leading: new Hero(
+        tag: index,
+        child: new CircleAvatar(
+          backgroundImage: new NetworkImage(friend.avatar),
         ),
-        title: new Text(friend.name),
-        subtitle: new Text(friend.email),
+      ),
+      title: new Text(friend.name),
+      subtitle: new Text(friend.email),
     );
   }
 
@@ -66,19 +68,20 @@ class _FriendsListPageState extends State<FriendsListPage> {
       ),
     );
   }
-  void findName(String placeName) async
-  {
+
+  void findName(String placeName) async {
     List<Friend> friendList = [];
-     for (int i = 0; i < friends.length; i++){
-      if (friends[i].name.contains(placeName)){
+    for (int i = 0; i < friends.length; i++) {
+      if (friends[i].name.contains(placeName)) {
         friendList.add(friends[i]);
       }
     }
-     print(friendList);
-     setState(() {
-       _friends = friendList;
-     });
+    print(friendList);
+    setState(() {
+      _friends = friendList;
+    });
   }
+
   TextEditingController searchEditingController = TextEditingController();
   Icon cusIcon = Icon(Icons.search);
   Widget cusSearchBar = Text("Friends");
@@ -96,22 +99,53 @@ class _FriendsListPageState extends State<FriendsListPage> {
       // );
       content = StreamBuilder<QuerySnapshot>(
         stream: FirebaseFirestore.instance.collection("friends").snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> querySnapshot){
-          if(querySnapshot.hasError){
+        builder:
+            (BuildContext context, AsyncSnapshot<QuerySnapshot> querySnapshot) {
+          if (querySnapshot.hasError) {
             return Text("Error");
           }
-          if(querySnapshot.connectionState == ConnectionState.waiting){
+          if (querySnapshot.connectionState == ConnectionState.waiting) {
             return CircularProgressIndicator();
-          }else{
-
+          } else {
             final list = querySnapshot.data.docs;
 
             return ListView.builder(
               itemCount: list.length,
-              itemBuilder: (context,index){
-                Friend currenFriend = new Friend(list[index]["avatar"], list[index]["name"], list[index]["email"], list[index]["location"]);
+              itemBuilder: (context, index) {
+                Friend currenFriend = new Friend(
+                    list[index]["avatar"],
+                    list[index]["name"],
+                    list[index]["email"],
+                    list[index]["location"]);
                 return ListTile(
                   onTap: () => _navigateToFriendDetails(currenFriend, index),
+                  onLongPress: () {
+                    showDialog(
+                      context: context,
+                      builder: (_) => new CupertinoAlertDialog(
+                        title: new Text("Delete"),
+                        content: new Text("Do you want to delete it?"),
+                        actions: <Widget>[
+                          FlatButton(
+                            child: Text('No'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          FlatButton(
+                            child: Text('Yes'),
+                            onPressed: () {
+                              print( list[index]["name"]);
+                              print( list[index]["email"]);
+                              print( list[index]["location"]);
+                              _loadFriends();
+                              Navigator.of(context).pop();
+                            },
+                          )
+                        ],
+                      )
+                    );
+                  },
                   leading: new Hero(
                     tag: index,
                     child: new CircleAvatar(
@@ -130,34 +164,35 @@ class _FriendsListPageState extends State<FriendsListPage> {
     return new Scaffold(
       appBar: new AppBar(
         title: cusSearchBar,
-          actions: <Widget>[
-            IconButton(
-                icon: cusIcon,
-                onPressed: (){
-                  setState(() {
-                    if(this.cusIcon.icon == Icons.search){
-                      this.cusIcon = Icon(Icons.cancel);
-                      this.cusSearchBar = TextField(
-                          onChanged: (val){
-                            findName(val);
-                          },
-                          controller: searchEditingController
-                      );
-                    }
-                    else{
-                      this.cusIcon = Icon(Icons.search);
-                      this.cusSearchBar = Text("Friends");
-                      _loadFriends();
-                    }
-                  });
-                }
-            )
-          ],
+        actions: <Widget>[
+          IconButton(
+              icon: cusIcon,
+              onPressed: () {
+                setState(() {
+                  if (this.cusIcon.icon == Icons.search) {
+                    this.cusIcon = Icon(Icons.cancel);
+                    this.cusSearchBar = TextField(
+                        onChanged: (val) {
+                          findName(val);
+                        },
+                        controller: searchEditingController);
+                  } else {
+                    this.cusIcon = Icon(Icons.search);
+                    this.cusSearchBar = Text("Friends");
+                    _loadFriends();
+                  }
+                });
+              })
+        ],
       ),
       body: content,
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => AddFriendPage(),));
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => AddFriendPage(),
+              ));
         },
         backgroundColor: Theme.of(context).primaryColor,
         child: Icon(Icons.add),
